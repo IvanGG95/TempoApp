f05c6c7a76fbb0ace9dcbcf3d6a35fba215f8e97
IvanGG95 <anacletillo95@gmail.com>
2021-05-23 14:07:46 +0200

    User - Controller

--- SpringTempo/src/main/java/com/TFG/tempo/data/repository/UserRepository.java
+++ SpringTempo/src/main/java/com/TFG/tempo/data/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.TFG.tempo.data.repository;
 
 import com.TFG.tempo.data.entities.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByUsername(String username);
+
+  User findUserByPersonInCharge(Long idPersonInCharge);
+
+  List<User> findUsersByPersonInChargeUsername(String name);
 }
--- SpringTempo/src/main/java/com/TFG/tempo/data/service/impl/UserServiceImpl.java
+++ SpringTempo/src/main/java/com/TFG/tempo/data/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.TFG.tempo.data.service.impl;
 
+import com.TFG.tempo.data.entities.Role;
 import com.TFG.tempo.data.entities.User;
 import com.TFG.tempo.data.repository.UserRepository;
 import com.TFG.tempo.data.service.api.UserService;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +20,36 @@ public class UserServiceImpl implements UserService {
   @Override
   public User findByUsername(String username) {
     return userRepository.findByUsername(username);
+  }
+
+  @Override
+  public User saveUser(User user) {
+    return userRepository.save(user);
+  }
+
+  @Override
+  public User findUserByPersonInCharge(Long idPersonInCharge) {
+    return userRepository.findUserByPersonInCharge(idPersonInCharge);
+  }
+
+  @Named("findById")
+  @Override
+  public User findById(Long id) {
+    return userRepository.findById(id).orElse(null);
+  }
+
+  @Named("findRoleByUserId")
+  @Override
+  public Collection<Role> findRoleByUserId(Long id) {
+    Optional<User> user = userRepository.findById(id);
+    return user.map(User::getRoles).orElse(null);
+  }
+
+  @Override
+  public List<User> findUsersByPersonInChargeName(String name) {
+    if (findByUsername(name) != null) {
+      return userRepository.findUsersByPersonInChargeUsername(name);
+    }
+    return null;
   }
 }
