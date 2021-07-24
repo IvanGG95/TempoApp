af80187bd24487c5669695beff0c8683f7bc0ecd
IvanGG95 <anacletillo95@gmail.com>
2021-07-24 22:04:54 +0200

    Changes

--- SpringTempo/src/main/java/com/TFG/tempo/data/repository/UserRepository.java
+++ SpringTempo/src/main/java/com/TFG/tempo/data/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.TFG.tempo.data.entities.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  @Transactional
   User findByUsername(String username);
 
   User findUserByPersonInCharge(Long idPersonInCharge);
